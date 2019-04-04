d8302631d9736e910e85826d388e3033e099a307
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-04-04 10:26:44 +0200

    Added tests

---
+++ tests/test_transpose_dict.py
@@ -0,0 +1,9 @@
+from transpose_dict import TD
+import json
+
+def test_transpose_dict():
+    with open("tests/start.json", "r") as f:
+        start = json.load(f)
+    for i in range(3):
+        with open("tests/test_{i}.json".format(i=i), "r") as f:
+            assert json.load(f) == TD(start, i)
